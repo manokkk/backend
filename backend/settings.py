6da609ac6b807b8e66782545aef41cb22728bf78
brybry20 <bryan123james@gmail.com>
2024-11-19 01:52:27 +0800

    tt

--- backend/settings.py
+++ backend/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+CORS_ALLOW_ALL_ORIGINS = True  # This allows all origins
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,5 +131,5 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
